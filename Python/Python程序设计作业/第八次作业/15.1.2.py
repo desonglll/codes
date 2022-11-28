f13a63a940311f3dbcb01945a1098ba42e89de7c
desonglll <lindesong666@163.com>
2022-11-28 22:26:57 +0800

    hello

---
+++ Python/Python程序设计作业/第八次作业/15.1.2.py
@@ -0,0 +1,11 @@
+s1 = 'red car'
+s2 = 'Pascal Case'
+s3 = 'python3.7'
+s4 = 'iPhoneX'
+
+print(s1.capitalize())
+print(s2.lower())
+print(s3.upper())
+print(s2.swapcase())
+print(s1.title())
+print(s4.casefold())
