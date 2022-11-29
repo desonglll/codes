eafce3929c616228bb9a5f596eac9213c2b9336d
desonglll <lindesong666@163.com>
2022-11-29 17:18:14 +0800

    2022-11-29 17:18:14

---
+++ Python/Python程序设计作业/第九次作业/csv_reader1.py
@@ -0,0 +1,11 @@
+import csv
+def readcsv1(csvfilepath):
+    with open(csvfilepath, newline='') as f:
+        f_csv = csv.reader(f)
+        headers = next(f_csv)
+        print(headers)
+        for row in f_csv:
+            print(row)
+
+if __name__ == '__main__':
+    readcsv1(r'./scores.csv')
--- deploy.sh
+++ deploy.sh
@@ -2,40 +2,6 @@
 time=$(date "+%Y-%m-%d %H:%M:%S")
 # echo -e "\nLast updated by **`whoami`** at: "${time} >> "README.md"
 
-# echo git diff
-
-echo -e "\`\`\`txt\n" >> changelog.md
-
-git diff >> changelog.md
-
-echo -e "\n\`\`\`" >> changelog.md
-
-# echo tree directories to tree.md
-
-echo -e "# Directories\n\n\`\`\`shell\n" > "TREE.md"
-
-tree >> "TREE.md"
-
-echo -e "\n\`\`\`" >> "TREE.md"
-
-# echo username and updated date to README.md
-
-echo -e "\nLast updated by **`whoami`** at: "${time} >> "README.md"
-
-# tree > "TREE.txt"
-
-# LINK
-
-# files=$(tree -if)
-# main=LINK.md
-# cat /dev/null > $main
-# for i in $files; do
-#   if [ $i != 'deploy.sh' ] && [ $i != '.gitignore' ]; then
-#     echo -e '['$i'](./'$i')\n' >> $main
-#   fi
-# done
-# echo '首页文件生成完毕'
-
 # github deploy
 
 git init
@@ -44,3 +10,5 @@ git commit -m "$(date "+%Y-%m-%d %H:%M:%S")"
 git branch -M main
 git remote add origin git@github.com:desonglll/codes.git
 git push -u origin main -f
+git remote add origin https://gitee.com/desonglll/code.git
+git push -u origin "master" -f
